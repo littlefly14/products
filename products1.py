d9a38d26fd6b21e26e2265cb7194ff9d1cae81f7
Amber lu <1114amber@gmail.com>
2022-04-15 21:30:17 +0800

    add for loop

---
+++ products1.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	products.append([name, price])
+print(products)
+
+products[0][0] #表大清單中第一個小清單的第一個位置
+
+for p in products:
+	print(p[0], "的價格是", p[1])
