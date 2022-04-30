d912b33331ba99e2ce35235b0a6812923fa04690
Amber lu <1114amber@gmail.com>
2022-04-30 17:13:15 +0800

    add header

--- products2.py
+++ products2.py
@@ -13,6 +13,7 @@ for p in products:
 	print(p[0], "的價格是", p[1])
 
 
-with open('products.csv', 'w') as f:  #csv是常用的檔案類型 可用excel開
+with open('products.csv', 'w', encoding='utf-8') as f:  #csv是常用的檔案類型 可用excel開
+	f.write('商品,價格\n')#寫入欄位名稱
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
